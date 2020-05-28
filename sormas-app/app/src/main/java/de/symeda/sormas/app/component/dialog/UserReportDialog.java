7f5449c038a6e40bcb3e43d812903b8102393cb4
barnabartha <barnabartha@she.net>
2020-05-28 15:55:27 +0300

    #2206 - applied code formatter from android studio(using eclipse code formatter)

--- sormas-app/app/src/main/java/de/symeda/sormas/app/component/dialog/UserReportDialog.java
+++ sormas-app/app/src/main/java/de/symeda/sormas/app/component/dialog/UserReportDialog.java
@@ -1,22 +1,21 @@
 /*
  * SORMAS® - Surveillance Outbreak Response Management & Analysis System
  * Copyright © 2016-2018 Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
- *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 package de.symeda.sormas.app.component.dialog;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 import android.content.Context;
 import android.os.AsyncTask;
@@ -25,9 +24,6 @@ import android.os.Bundle;
 import androidx.databinding.ViewDataBinding;
 import androidx.fragment.app.FragmentActivity;
 
-import com.google.firebase.analytics.FirebaseAnalytics;
-
-import de.symeda.sormas.app.BaseActivity;
 import de.symeda.sormas.app.R;
 import de.symeda.sormas.app.SormasApplication;
 import de.symeda.sormas.app.core.BoolResult;
@@ -38,78 +34,83 @@ import de.symeda.sormas.app.core.async.TaskResultHolder;
 import de.symeda.sormas.app.core.notification.NotificationHelper;
 import de.symeda.sormas.app.core.notification.NotificationType;
 import de.symeda.sormas.app.databinding.DialogUserReportLayoutBinding;
-import de.symeda.sormas.app.util.Callback;
 
 public class UserReportDialog extends AbstractDialog {
 
-    public static final String TAG = UserReportDialog.class.getSimpleName();
+	public static final String TAG = UserReportDialog.class.getSimpleName();
 
-    private AsyncTask dialogTask;
-    private String uuid;
-    private String viewName;
-    private DialogUserReportLayoutBinding contentBinding;
+	private AsyncTask dialogTask;
+	private String uuid;
+	private String viewName;
+	private DialogUserReportLayoutBinding contentBinding;
 
-    // Constructor
+	// Constructor
 
-    public UserReportDialog(final FragmentActivity activity, String viewName, String uuid) {
-        super(activity, R.layout.dialog_root_layout, R.layout.dialog_user_report_layout,
-                R.layout.dialog_root_two_button_panel_layout, R.string.heading_user_report,
-                R.string.info_user_report);
+	public UserReportDialog(final FragmentActivity activity, String viewName, String uuid) {
+		super(
+			activity,
+			R.layout.dialog_root_layout,
+			R.layout.dialog_user_report_layout,
+			R.layout.dialog_root_two_button_panel_layout,
+			R.string.heading_user_report,
+			R.string.info_user_report);
 
-        this.uuid = uuid;
-        this.viewName = viewName;
-    }
+		this.uuid = uuid;
+		this.viewName = viewName;
+	}
 
-    // Overrides
+	// Overrides
 
-    @Override
-    protected void setContentBinding(Context context, ViewDataBinding binding, String layoutName) {
-        this.contentBinding = (DialogUserReportLayoutBinding) binding;
-    }
+	@Override
+	protected void setContentBinding(Context context, ViewDataBinding binding, String layoutName) {
+		this.contentBinding = (DialogUserReportLayoutBinding) binding;
+	}
 
-    @Override
-    protected void initializeContentView(ViewDataBinding rootBinding, ViewDataBinding buttonPanelBinding) {
-        setPositiveCallback(() -> {
-            DefaultAsyncTask executor = new DefaultAsyncTask(getContext()) {
-                @Override
-                public void doInBackground(TaskResultHolder resultHolder) {
-                    try {
-                        String description = contentBinding.userReportMessage.getValue();
+	@Override
+	protected void initializeContentView(ViewDataBinding rootBinding, ViewDataBinding buttonPanelBinding) {
+		setPositiveCallback(() -> {
+			DefaultAsyncTask executor = new DefaultAsyncTask(getContext()) {
 
-                        Bundle eventBundle = new Bundle();
-                        eventBundle.putString(FirebaseAnalytics.Param.CONTENT, description);
-                        ((SormasApplication) getActivity().getApplication()).getFirebaseAnalytics().logEvent(FirebaseEvent.USER_ERROR_REPORT, eventBundle);
+				@Override
+				public void doInBackground(TaskResultHolder resultHolder) {
+					try {
+						String description = contentBinding.userReportMessage.getValue();
 
-                        resultHolder.setResultStatus(new BoolResult(true,  getActivity().getResources().getString(R.string.message_report_sent)));
-                    } catch (Exception ex) {
-                        resultHolder.setResultStatus(new BoolResult(true, getActivity().getResources().getString(R.string.message_report_not_sent)));
-                    }
-                }
+						Bundle eventBundle = new Bundle();
+						eventBundle.putString(FirebaseAnalytics.Param.CONTENT, description);
+						((SormasApplication) getActivity().getApplication()).getFirebaseAnalytics()
+							.logEvent(FirebaseEvent.USER_ERROR_REPORT, eventBundle);
 
-                @Override
-                public void onPostExecute(AsyncTaskResult<TaskResultHolder> taskResult) {
-                    BoolResult resultStatus = taskResult.getResultStatus();
-                    if (taskResult.getResultStatus().isSuccess()) {
-                        NotificationHelper.showDialogNotification(UserReportDialog.this, NotificationType.SUCCESS, resultStatus.getMessage());
-                    } else {
-                        NotificationHelper.showDialogNotification(UserReportDialog.this, NotificationType.ERROR, resultStatus.getMessage());
-                    }
-                }
-            };
+						resultHolder.setResultStatus(new BoolResult(true, getActivity().getResources().getString(R.string.message_report_sent)));
+					} catch (Exception ex) {
+						resultHolder.setResultStatus(new BoolResult(true, getActivity().getResources().getString(R.string.message_report_not_sent)));
+					}
+				}
 
-            dialogTask = executor.executeOnThreadPool();
-        });
+				@Override
+				public void onPostExecute(AsyncTaskResult<TaskResultHolder> taskResult) {
+					BoolResult resultStatus = taskResult.getResultStatus();
+					if (taskResult.getResultStatus().isSuccess()) {
+						NotificationHelper.showDialogNotification(UserReportDialog.this, NotificationType.SUCCESS, resultStatus.getMessage());
+					} else {
+						NotificationHelper.showDialogNotification(UserReportDialog.this, NotificationType.ERROR, resultStatus.getMessage());
+					}
+				}
+			};
 
-        setNegativeCallback(() -> {
-            if (dialogTask != null && !dialogTask.isCancelled()) {
-                dialogTask.cancel(true);
-            }
-        });
-    }
+			dialogTask = executor.executeOnThreadPool();
+		});
 
-    @Override
-    public int getPositiveButtonText() {
-        return R.string.action_send;
-    }
+		setNegativeCallback(() -> {
+			if (dialogTask != null && !dialogTask.isCancelled()) {
+				dialogTask.cancel(true);
+			}
+		});
+	}
+
+	@Override
+	public int getPositiveButtonText() {
+		return R.string.action_send;
+	}
 
 }
